469ab7b09e2f7a84cc008118be8aeef640a48776
atsu0905 <amamstudyut@gmail.com>
2021-11-27 18:53:18 +0900

    SIZEの変更の反映

--- cal_best.c
+++ cal_best.c
@@ -8,9 +8,9 @@
 #include <stdlib.h>
 
 DataItem** calculate_best(DataItem **graph_table,DataItem **inv_graph_table,DataItem **condition_table, DataItem **edge_num_table){
-    DataItem *next_condition_table[SIZE*2];
-    DataItem *max_transition_end_table[SIZE*2];
-    DataItem *best_table[SIZE*2];
+    DataItem *next_condition_table[SIZE];
+    DataItem *max_transition_end_table[SIZE];
+    DataItem *best_table[SIZE];
 
     int w1,w2,w3,b1,b2,b3;
     int *board_num_array[6] = {&w1,&w2,&w3,&b1,&b2,&b3};
