6b66583da3a35e61106e108bfaa3f3c1940cc72b
root2116 <root2116@icloud.com>
2021-12-01 01:20:34 +0900

    mainを整えた

--- src/main.c
+++ src/main.c
@@ -11,18 +11,22 @@
 
 
 int main(int argc,char *argv[]){
+    // 一回目の実行でアンコメントする
+    // generate_and_save_set(); 
 
-
-    generate_and_save_best_move_array();
-    // short int *best_move_array = (short int*)calloc(SIZE,sizeof(short int));
+    // 二回目の実行でアンコメントする
+    // generate_and_save_best_move_array();
+    
+    
+    short int *best_move_array = (short int*)calloc(SIZE,sizeof(short int));
    
-    // load_short_array_from_file(best_move_array,BEST_MOVE_ARRAY_PATH);
+    load_short_array_from_file(best_move_array,BEST_MOVE_ARRAY_PATH);
 
-    // DataItem *dict = (DataItem *)calloc(SIZE, sizeof(DataItem));
+    DataItem *dict = (DataItem *)calloc(SIZE, sizeof(DataItem));
 
-    // load_hash_table_from_file((DataItem**)dict,DICT_PATH);
+    load_hash_table_from_file((DataItem**)dict,DICT_PATH);
 
-    // start_game(atoi(argv[1]),(DataItem**)dict,best_move_array);
+    start_game(atoi(argv[1]),(DataItem**)dict,best_move_array);
 
     return 0;
 }
